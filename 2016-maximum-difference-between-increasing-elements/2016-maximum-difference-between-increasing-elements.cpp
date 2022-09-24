dffe034336c03f6539b571146a6480e2bc597767
Debjyoti Kumar Gupta <90954339+DebjyotiKumarGupta@users.noreply.github.com>
2022-09-24 23:11:25 +0530

    Time: 4 ms (79.58%), Space: 8.4 MB (37.65%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int maximumDifference(vector<int>& p) {
-        int sum = -1, res = 0;
-        for(int i =0;i<p.size();i++){
-            for(int j=i+1;j<p.size();j++){
-                if(p[j]>p[i]){
-                res = abs(p[i]-p[j]);
-                sum = max(res , sum);
-                }
-            }
+    int maximumDifference(vector<int>& nums) {
+        int res = 0,sum =nums[0];
+        for(int i =1;i<nums.size();i++){
+            res = max(res,nums[i]-sum);
+            sum = min(nums[i],sum);
         }
-        return sum;
+        if(res<=0) return -1;
+        
+        return res;
     }
 };
