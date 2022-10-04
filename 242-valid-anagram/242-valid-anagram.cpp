ca05f2e2e2b7833f5414ef1c2874398939bdc90e
Debjyoti Kumar Gupta <90954339+DebjyotiKumarGupta@users.noreply.github.com>
2022-10-04 15:34:56 +0530

    Time: 16 ms (59.21%), Space: 7.4 MB (55.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-      sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return (s==t);
+    const int CHAR = 256;
+        int count[CHAR]={0};
+        int n = s.length(), m = t.length();
+        if(n!=m) return 0;
+        for(int i=0;i<m;i++){
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+        for(int i=0;i<n;i++){
+            if(count[t[i]]!=0){
+                return 0;
+            }
+        }
+        return 1;
     }
 };
