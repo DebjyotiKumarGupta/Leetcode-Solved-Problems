fb4399116dfecd21eecc8274df34393c9c684f77
Debjyoti Kumar Gupta <90954339+DebjyotiKumarGupta@users.noreply.github.com>
2022-10-04 21:05:00 +0530

    Time: 80 ms (47.21%), Space: 23.3 MB (47.38%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -6,8 +6,8 @@ public:
         for(int i=0;i<names.size();i++){
              v.push_back( make_pair(heights[i],names[i]) );
         }
-        sort(v.begin(),v.end());
-        reverse(v.begin(),v.end());
+        sort(v.begin(),v.end(),greater<pair<int,string>>());
+        // reverse(v.begin(),v.end());
         for(int i=0;i<names.size();i++){
             s.push_back(v[i].second);
         }
