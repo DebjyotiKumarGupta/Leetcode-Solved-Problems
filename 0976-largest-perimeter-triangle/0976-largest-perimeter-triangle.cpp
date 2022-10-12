7dd8604bd45f81a8d9a7676ab78e413361f0f31b
Debjyoti Kumar Gupta <90954339+DebjyotiKumarGupta@users.noreply.github.com>
2022-10-12 12:08:54 +0530

    Time: 43 ms (87.82%), Space: 21.9 MB (22.88%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -6,6 +6,6 @@ public:
         for(int i=0;i<nums.size()-2;i++){
             if(nums[i]<nums[i+2]+nums[i+1]) return nums[i]+nums[i+1]+nums[i+2];
         }
-        return ans;
+        return 0;
     }
 };
