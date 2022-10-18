c96013174492812c14627491ce6dcf40431290e6
Debjyoti Kumar Gupta <90954339+DebjyotiKumarGupta@users.noreply.github.com>
2022-10-18 12:49:59 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (19.16%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-       int x = num;
-        int sum=0;
-        while(x!=0){
-            sum = sum*10 + (x%10);
-            x/=10;
-        }
-          while(sum!=0){
-            x = x*10 + (sum%10);
-            sum/=10;
-        }
-        return (x==num);
+      if(num==0) return true;
+      else if(num%10==0) return false;
+      else return true;
     }
 };
